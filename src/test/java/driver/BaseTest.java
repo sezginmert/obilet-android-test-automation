55b06954906b5fabc0c7c1ccd20b48cd3a0678fb
Jenkins CI <jenkins@example.com>
2025-06-19 22:59:36 +0300

    test/CurrencySwitcherTest.oguz

--- src/test/java/driver/BaseTest.java
+++ src/test/java/driver/BaseTest.java
@@ -2,16 +2,21 @@ package driver;
 
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import utilities.LoggerHelper;
 
 public class BaseTest {
 
+    LoggerHelper loggerHelper = new LoggerHelper();
+
     @BeforeClass
-    public void setup(){
+    public void setup() {
         Driver.getAppiumDriver();
+        loggerHelper.info("Appium driver initialized before the test class.");
     }
 
     @AfterClass
-    public void teardown(){
+    public void teardown() {
         Driver.quitAppiumDriver();
+        loggerHelper.info("Appium driver terminated after the test class.");
     }
 }
